7b9f74dc27bbcfc9e040353c6cf871f37dacfdf5
Valentino Behret <behret@hm.edu>
2024-09-25 17:50:14 +0200

    Ignore metadata.yaml files

--- lint.sh
+++ lint.sh
@@ -3,9 +3,9 @@ set -e
 
 # Local variables
 if [ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" ]; then
-	project_root="$(git rev-parse --show-toplevel)"
+  project_root="$(git rev-parse --show-toplevel)"
 else
-	project_root="$(pwd)"
+  project_root="$(pwd)"
 fi
 
 # Overwrite options
@@ -13,61 +13,61 @@ python_root="$project_root"
 cpp_root="$project_root"
 shell_root="$project_root"
 for i in "$@"; do
-	case $1 in
-	--python=*)
-		python_root="${project_root}/${i#*=}"
-		shift
-		;;
-	--cpp=*)
-		cpp_root="${project_root}/${i#*=}"
-		shift
-		;;
-	--shell=*)
-		shell_root="${project_root}/${i#*=}"
-		shift
-		;;
-	--*)
-		echo "Unknown option $i"
-		exit 1
-		;;
-	*) ;;
-	esac
+  case $1 in
+  --python=*)
+    python_root="${project_root}/${i#*=}"
+    shift
+    ;;
+  --cpp=*)
+    cpp_root="${project_root}/${i#*=}"
+    shift
+    ;;
+  --shell=*)
+    shell_root="${project_root}/${i#*=}"
+    shift
+    ;;
+  --*)
+    echo "Unknown option $i"
+    exit 1
+    ;;
+  *) ;;
+  esac
 done
 
 # Functions
 ok() {
-	printf "\e[32mok\e[0m\n"
+  printf "\e[32mok\e[0m\n"
 }
 
 header() {
-	printf "\e[34mCheck \e[36m%s\e[34m files with '%s'...\e[0m" "$1" "$2"
+  printf "\e[34mCheck \e[36m%s\e[34m files with '%s'...\e[0m" "$1" "$2"
 }
 
 skip() {
-	printf "\e[33mskip\e[0m\n"
+  printf "\e[33mskip\e[0m\n"
 }
 
 line() {
-	# print horizontal line
-	printf "\e[34m%s\e[0m\n" "----------------------------------------"
+  # print horizontal line
+  printf "\e[34m%s\e[0m\n" "----------------------------------------"
 }
 
 append_if_exists() {
-	if [ -d "${project_root}/$1" ]; then
-		printf "%s/%s" "$project_root" "$1"
-	else
-		printf "%s" "$project_root"
-	fi
+  if [ -d "${project_root}/$1" ]; then
+    printf "%s/%s" "$project_root" "$1"
+  else
+    printf "%s" "$project_root"
+  fi
 }
 
 # If input arg set to project root
 line
 if [ -f "${python_root}/pyproject.toml" ]; then
-	project_name=$(python3 -c "import toml; print(toml.load('pyproject.toml')['project']['name'])")
-	python_root=$(append_if_exists "$project_name")
-	printf "New python root from pyproject.toml: %s\n" "$python_root"
+  project_name=$(python3 -c "import toml; print(toml.load('pyproject.toml')['project']['name'])")
+  python_root=$(append_if_exists "$project_name")
+  printf "New python root from pyproject.toml: %s\n" "$python_root"
 else
-	python_root="$(append_if_exists "src")"
+  python_root="$(append_if_exists "src")"
 fi
 shell_root="$(append_if_exists "scripts")"
 cpp_root="$(append_if_exists "src")"
@@ -86,62 +86,62 @@ n_docker="$(find "$python_root" -name "*Dockerfile*" | wc -l)"
 # Linter: shellcheck
 header "$n_shell" "shellcheck"
 if [ "$n_shell" -gt 0 ]; then
-	find "$shell_root" -name "*.sh" -print0 | xargs -0 shellcheck -e SC1090 -e SC1091
-	ok
+  find "$shell_root" -name "*.sh" -print0 | xargs -0 shellcheck -e SC1090 -e SC1091
+  ok
 else
-	skip
+  skip
 fi
 
 if [ "$n_cpp" -gt 0 ]; then
-	# Linter: cpplint
-	header "$n_cpp" "cpplint"
-	cpplint --recursive --quiet --filter=-build/c++11,-runtime/explicit "$cpp_root"
-	ok
+  # Linter: cpplint
+  header "$n_cpp" "cpplint"
+  cpplint --recursive --quiet --filter=-build/c++11,-runtime/explicit "$cpp_root"
+  ok
 
-	# Linte: cppcheck
-	header "$n_cpp" "cppcheck"
-	cppcheck --quiet --enable=warning,style,performance,portability "$cpp_root"
-	ok
+  # Linte: cppcheck
+  header "$n_cpp" "cppcheck"
+  cppcheck --quiet --enable=warning,style,performance,portability "$cpp_root"
+  ok
 else
-	header "$n_cpp" "cpp"
-	skip
+  header "$n_cpp" "cpp"
+  skip
 fi
 
 if [ "$n_python" -gt 0 ]; then
-	# Linter: pylint
-	header "$n_python" "pylint"
-	pylint --recursive=true --score=n "$python_root"
-	ok
+  # Linter: pylint
+  header "$n_python" "pylint"
+  pylint --recursive=true --score=n "$python_root"
+  ok
 
-	# Linter: flake8
-	header "$n_python" "flake8"
-	flake8 --append-config="/etc/flake8" "$project_root"
-	ok
+  # Linter: flake8
+  header "$n_python" "flake8"
+  flake8 --append-config="/etc/flake8" "$project_root"
+  ok
 else
-	header "$n_python" "python"
-	skip
+  header "$n_python" "python"
+  skip
 fi
 
 # Linter: yamllint
 header "$n_yaml" "yamllint"
 if [[ $n_yaml -gt 0 ]]; then
-	yamllint "$project_root"
-	ok
+  yamllint -c /etc/yamllint "$project_root"
+  ok
 else
-	skip
+  skip
 fi
 
 # Linter: hadolint
 header "$n_docker" "hadolint"
 if [[ $n_docker -gt 0 ]]; then
-	find "$project_root" -name "*Dockerfile*" -print0 | xargs -0 hadolint \
-		--ignore DL3006 \
-		--ignore DL3008 \
-		--ignore DL3033 \
-		--ignore SC1091
-	ok
+  find "$project_root" -name "*Dockerfile*" -print0 | xargs -0 hadolint \
+    --ignore DL3006 \
+    --ignore DL3008 \
+    --ignore DL3033 \
+    --ignore SC1091
+  ok
 else
-	skip
+  skip
 fi
 
 line
